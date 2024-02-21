eda2f321b7750638941f307095010b0b734b285a
freetimezp <kraftukrnet@ukr.net>
2024-02-21 15:10:12 +0200

    02. js-form-02-login-sqldb. Base setup. Routing system.

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><Login /></div>
+  },
+  {
+    path: '/register',
+    element: <div><Register /></div>
+  },
+  {
+    path: '/dashboard',
+    element: <div><Dashboard /></div>
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
